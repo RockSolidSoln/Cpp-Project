13d2f74433089b1a99df827fcecda17b56cc8d51
Samaxy <salah322s1@gmail.com>
2021-10-06 11:31:55 +0800

    login function

---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    vector<tuple<string,int,string,int>> users;
+    tuple<string,int,string,int> userdata;
+
+    ifstream file;
+    file.open("users.dat");
+    while (file>>get<0>(userdata))
+    {
+        file >>get<1>(userdata);
+        file >>get<2>(userdata);
+        file >>get<3>(userdata);
+
+        users.push_back(userdata);
+
+    }
+
+    file.close();
+
+    string name, pass;
+    cin >> name;
+    cin >> pass;
+
+    for (auto elem: users)
+    {
+         if (name == get<0>(elem) &&
+             pass == get<2>(elem) &&
+             get<3>(elem)==1)
+        {
+            cout << get<0>(elem) << "; "
+                 << ((get<1>(elem)==1)?"admin":"user")
+                 << ": " <<get<2>(elem) << ": "
+                 << ((get<3>(elem)==1)?"Active":"Deleted")
+                 << endl;
+
+
+        }     
+
+
+
+
+
+
+
+
+
+
+
+
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
