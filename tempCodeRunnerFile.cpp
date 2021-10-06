2d54e40bf896dc4d76e8332d6041de47bffdb483
Ayaan Ahmad <76241848+ayaan278@users.noreply.github.com>
2021-10-06 21:30:10 +0400

    added deletion function

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,11 +1,1 @@
-  // // cin >> ch;
-    // switch(ch)
-    // {
-    //     case(1):break;
-    //     case(2):break;
-    //     case(3):break;
-    //     case(4):break;
-    //     case(5):break;
-
-    //     default:
-    // }
+users.push_back(elem);
