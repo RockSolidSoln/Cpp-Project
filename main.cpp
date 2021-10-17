5f4245728d4b90f1efa6c399467908bad14d61bf
Samaxy <salah322s1@gmail.com>
2021-10-17 09:54:38 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -411,5 +411,33 @@ void logrecord(string str)
 // --------------------This function shows different functions for user to perform------------------------------
 void viewfunc()
 {
-    // blank for now
+    int ch;
+
+cout <<"--------------------------------------------------------\n"
+     <<"|        please enter a number to procced:             |\n"
+     <<"--------------------------------------------------------\n"
+     <<"|  Enter 1 to find the minmum value:                   |\n"
+     <<"|  Enter 2 to find the maximum value:                  |\n"
+     <<"|  Enter 3 to find the median of the value:            |\n"
+     <<"|  Enter 4 to find the mean of the value:              |\n"
+     <<"|  Enter 5 to find the variance of the value:          |\n"
+     <<"|  Enter 6 to find the standard deviation of the value |\n"   
+     <<"--------------------------------------------------------\n"
+     <<endl;
+    cin>ch;
+    switch(ch)
+    {
+         case(1)    : findMin()
+                         break;
+         case(2)    : findMax()
+                         break;
+         case(3)    : findMed()                
+                         break;
+         case(4)    : findMean()
+                         break;
+         case(5)    : findVar()                                
+                         break;
+         case(6)    : findDev()
+                         break;                
+    }
 }
