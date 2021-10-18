1a24bd604a3ce0dadd5667630ef4bc3366108d46
Samaxy <salah322s1@gmail.com>
2021-10-18 15:40:52 +0800

    .

--- histogram.cpp
+++ histogram.cpp
@@ -8,6 +8,51 @@
 #include <vector>
 
 using namespace std;
+vec1 array1()
+{
+    string arr1[100];
+    int arr2[100][5];
+    int k=0;
+    int i=0;
+    ifstream data;
+    data.open("StudentDataBase.dat");
+
+    while (!data.eof()){
+        string x;
+        data >> x;
+        arr1[i] = x;
+        i++;
+
+    }
+
+    for (int i=0; i<100; i++){
+        string s = arr1[i];
+        stringstream string_stream(s);
+        int i2 = 0;
+
+        while(string_stream.good()){
+            string y;
+            getline(string_stream, y, ',' );
+            stringstream string_stream2(y);
+            string_stream2 >> k;
+            arr2[i][i2]=k;
+            i2++;
+        }
+    }
+
+    vec1 vee;
+    array <int,5> a;
+    for(int i=0;i<100;i++)
+    {
+        get<0>(a)=arr2[i][0];
+        get<1>(a)=arr2[i][1];
+        get<2>(a)=arr2[i][2];
+        get<3>(a)=arr2[i][3];
+        get<4>(a)=arr2[i][4];
+        vee.push_back(a);
+    }
+    return vee;
+}
 
 int main()
 {
