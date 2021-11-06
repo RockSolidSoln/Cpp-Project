1985deb2dd6abb41eb9042ee291f738b62d652aa
Ayaan Ahmad <76241848+ayaan278@users.noreply.github.com>
2021-11-06 21:01:26 +0400

    reading file done

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,35 @@
-vec1
+string str1;
+    // double db=0;
+    // double ds=0;
+    // char ch;
+    // cout <<"--------------------------------------------------------\n"
+    //     <<"          "<<username<<" ,Please enter a number to procced:\n"
+    //     <<"--------------------------------------------------------\n"
+    //     <<"|  Enter 1 to create a report as a text file           |\n"
+    //     <<"|  Enter 2 to create a HTML report                     |\n"
+    //     <<"|  Enter B to go back to perform more functions        |\n"
+    //     <<"|  Enter U to go back to User's settings menu          |\n"
+    //     <<"|  Enter 0 to exit                                     |\n"
+    //     <<"--------------------------------------------------------\n";
+    // cin>>ch;
+    // cin.ignore(' ','\n');
+    // string str=" choose the report's menu option ";
+    // str.push_back(ch);
+    // logrecord(str);
+    // switch(ch)
+    // {
+    //      case('1')    : savereport(1,str1,db,ds);
+    //                      break;
+    //      case('2')    : saveHTMLreport(1,str1,db,ds);
+    //                      break;
+    //      case('B')    : //viewfunc();
+    //                      break;
+    //      case('U')    : getchoice();
+    //                      break;
+    //      case('0')    : exit(0);
+    //                      break;               
+    //      default: cout<<"Wrong choice------------->\n"
+    //                     <<"Please Enter from the choice given below\n";
+    //             reportsmenu();
+    //             break;         
+    // }
