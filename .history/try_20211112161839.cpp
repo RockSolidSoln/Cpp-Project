c3c5cb13d394ee51bbff10dc98068d46018e10c6
Ayaan Ahmad <76241848+ayaan278@users.noreply.github.com>
2021-11-12 16:58:24 +0400

    median done

---
+++ .history/try_20211112161839.cpp
@@ -0,0 +1,17 @@
+// ----------------------------------------start of the program---------------------------------------------
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <tuple>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
---
+++ .history/try_20211112161955.cpp
@@ -0,0 +1,24 @@
+// ----------------------------------------start of the program---------------------------------------------
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <tuple>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main()
+{   
+    int ar=[3,2,2,3,6];
+    for(int i=0;i<5;i++)
+    {
+        cout<<ar
+    }
+    int t= sizeof(ar)/sizeof(ar[0]);
+        sort(ar, ar+t);
+    return 0;
+}
