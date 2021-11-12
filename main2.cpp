8bb31d8b353e7ed5d5e401d600535771119a5783
Samaxy <salah322s1@gmail.com>
2021-11-12 08:50:21 +0800

    reportsmenu changed

--- main2.cpp
+++ main2.cpp
@@ -1116,40 +1116,40 @@ void printhist(struct datavalues d, int &lowlimit, int &upperlimit,const int col
 //------------------------------This functions shows the report menu-----------------------------------------
 void reportsmenu()
 {   
-    // string str1;
-    // double db=0;
-    // double ds=0;
-    // char ch;
-    // cout <<"--------------------------------------------------------\n"
-    //     <<"          "<<username<<" ,Please enter a number to procced:\n"
-    //     <<"--------------------------------------------------------\n"
-    //     <<"|  Enter 1 to create a report as a text file           |\n"
-    //     <<"|  Enter 2 to create a HTML report                     |\n"
-    //     <<"|  Enter B to go back to perform more functions        |\n"
-    //     <<"|  Enter U to go back to User's settings menu          |\n"
-    //     <<"|  Enter 0 to exit                                     |\n"
-    //     <<"--------------------------------------------------------\n";
-    // cin>>ch;
-    // cin.ignore(' ','\n');
-    // string str=" choose the report's menu option ";
-    // str.push_back(ch);
-    // logrecord(str);
-    // switch(ch)
-    // {
-    //      case('1')    : savereport(1,str1,db,ds);
-    //                      break;
-    //      case('2')    : saveHTMLreport(1,str1,db,ds);
-    //                      break;
-    //      case('B')    : viewfunc();
-    //                      break;
-    //      case('U')    : getchoice();
-    //                      break;
-    //      case('0')    : exit(0);
-    //                      break;               
-    //      default: cout<<"Wrong choice------------->\n"
-    //                     <<"Please Enter from the choice given below\n";
-    //             reportsmenu();
-    //             break;         
-    // }
+    string str1;
+    double db=0;
+    double ds=0;
+    char ch;
+    cout <<"--------------------------------------------------------\n"
+        <<"          "<<username<<" ,Please enter a number to procced:\n"
+        <<"--------------------------------------------------------\n"
+        <<"|  Enter 1 to create a report as a text file           |\n"
+        <<"|  Enter 2 to create a HTML report                     |\n"
+        <<"|  Enter B to go back to perform more functions        |\n"
+        <<"|  Enter U to go back to User's settings menu          |\n"
+        <<"|  Enter 0 to exit                                     |\n"
+        <<"--------------------------------------------------------\n";
+    cin>>ch;
+    cin.ignore(' ','\n');
+    string str=" choose the report's menu option ";
+    str.push_back(ch);
+    logrecord(str);
+    switch(ch)
+    {
+         case('1')    : savereport(1,str1,db,ds);
+                         break;
+         case('2')    : saveHTMLreport(1,str1,db,ds);
+                         break;
+         case('B')    : usermenu();
+                         break;
+         case('U')    : getchoice();
+                         break;
+         case('0')    : exit(0);
+                         break;               
+         default: cout<<"Wrong choice------------->\n"
+                        <<"Please Enter from the choice given below\n";
+                reportsmenu();
+                break;         
+    }
     
 }
